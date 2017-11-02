98c4f585af591c80d6b6f01347d09fa01a4b9c6a
Dimitrii Pokrovskii <dimitrii@engineering-bear.com>
2017-11-02 13:51:12 -0700

    Updated Audio List to randomize order.

--- campaigns/static.curiousear.com/js/main.js
+++ campaigns/static.curiousear.com/js/main.js
@@ -11,6 +11,25 @@
   var AudioList;
   (function () {
 
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
+
     function getJSON (url, cb) {
       $.getJSON( url, function( data ) {
         cb(data);
@@ -23,7 +42,8 @@
     function dataConverter (dataIn) {
       var dataOut = [];
       if (dataIn instanceof Array) {
-        dataIn.forEach(function (model) {
+        shuffledDataIn = shuffle(dataIn);
+        shuffledDataIn.forEach(function (model) {
           dataOut.push(model);
         });
       }
--- src/js/featured_audio_list.js
+++ src/js/featured_audio_list.js
@@ -5,31 +5,31 @@
   // Globals -------------------------------------------------------------------
   //
   var AUDIO_JSON_URL = "http://api.curiousear.com/audio/featured";
-
-  function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  }
-
+  
   //
   // AudioPlayer ---------------------------------------------------------------
   //
   var AudioList;
   (function () {
+
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
 
     function getJSON (url, cb) {
       $.getJSON( url, function( data ) {
