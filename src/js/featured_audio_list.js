b64a8208e307a730d55eb174f38370f545a5e8db
Dimitrii Pokrovskii <dimitrii@engineering-bear.com>
2017-11-02 13:24:11 -0700

    Add a random shuffled to featured stories.

--- src/js/featured_audio_list.js
+++ src/js/featured_audio_list.js
@@ -5,6 +5,26 @@
   // Globals -------------------------------------------------------------------
   //
   var AUDIO_JSON_URL = "http://api.curiousear.com/audio/featured";
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
   //
   // AudioPlayer ---------------------------------------------------------------
   //
@@ -23,7 +43,8 @@
     function dataConverter (dataIn) {
       var dataOut = [];
       if (dataIn instanceof Array) {
-        dataIn.forEach(function (model) {
+        shuffledDataIn = shuffle(dataIn);
+        shuffledDataIn.forEach(function (model) {
           dataOut.push(model);
         });
       }
