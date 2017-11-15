8efe332ebb7f9c35a168633b36718976907cbc9d
Keenan <keenan.wells@emergeinteractive.com>
2017-11-15 10:05:45 -0800

    component serperated out with own gulp file

---
+++ audio_component/gulpfile.js
@@ -0,0 +1,82 @@
+// Required Plugins
+const gulp = require('gulp'),
+    imagemin     = require('gulp-imagemin'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer'),
+    notify = require('gulp-notify'),
+    del = require('del'),
+    runSequence = require('run-sequence'),
+    concat = require('gulp-concat');
+
+//audio component variables
+const dist = './dist/',
+    src = './src/',
+    jsSrc = src + 'js/*.js',
+    jsVendorSrc = src + 'js/vendor/*js',
+    jsDist = dist + 'js/',
+    scssSrc = src + 'scss/*.scss',
+    cssDist = dist + 'css/',
+    imgSrc = src + 'img',
+    imgDest = dist + 'img/';
+
+
+function handleErrors(error) {
+    notify().write(error);
+    console.log(error);
+    process.exit(1);
+}
+
+// Compile Sass for Development and Production
+// Autoprefixes browser prefixes
+gulp.task('sass', function() {
+    return gulp.src(scssSrc)
+        .pipe(sourcemaps.init())
+        // .pipe(gulpif(isProd, sourcemaps.init()))
+        .pipe(sass({
+            sourceComments : 'map',
+            outputStyle: 'nested'
+        }))
+        .on('error', handleErrors)
+        .pipe(postcss([autoprefixer({
+            browsers: ['last 2 versions']
+        })]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(cssDist));
+});
+
+// Move and compress images
+gulp.task('imgProcess', function() {
+    gulp.src(imgSrc)
+        .pipe(imagemin())
+        .pipe(gulp.dest(imgDest));
+});
+
+
+// Move and compress JavaScript
+gulp.task('js', function() {
+    gulp.src([ jsVendorSrc , jsSrc])
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(jsDist));
+});
+
+
+// Clean up desitination directory
+gulp.task('clean', function() {
+    return del([dist]);
+});
+
+// Files are automatically watched
+gulp.task('watch', function() {
+    gulp.watch(scssSrc, ['sass']);
+    gulp.watch([jsSrc], ['js']);
+});
+
+// Gulp development task
+gulp.task('dev', ['clean', ], function(cb) {
+    cb = cb || function() {};
+    return runSequence(['sass', 'js', 'imgProcess'], 'watch', cb);
+});
