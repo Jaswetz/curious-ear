6e30f749fb3f874e14ce9f0c6093d1426ad24244
Keenan <keenan.wells@emergeinteractive.com>
2017-11-20 19:28:13 -0800

    added some stuff

--- audio_component/src/js/audio_controller.js
+++ audio_component/src/js/audio_controller.js
@@ -27,14 +27,24 @@ var audioController = (function($) {
 
         animateIntroStart() {
             $('#audio-element--button-init').fadeOut(500, () => {
-                introBall = anime({
+                var imageChange = anime({
                     targets: '#wave-form-sphere',
-                    width: '500px',
-                    height: '500px',
+                    background: 'url("/audio_component/dist/img/img--mouth.jpg")',
                     duration: '1000',
                     easing: 'linear',
-                    complete: () => controller.appendControls()
+                    complete: () => introBallGrow()
                 });
+
+                var introBallGrow = () => {
+                    var introBall = anime({
+                        targets: '#wave-form-sphere',
+                        width: '500px',
+                        height: '500px',
+                        duration: '1000',
+                        easing: 'linear',
+                        complete: () => controller.appendControls()
+                    });
+                }
             });
 
         },
@@ -98,39 +108,39 @@ var audioController = (function($) {
             controller.loadAudio(prevAudio);
         },
 
-        expandControls() {
-            $('#controls-expander').fadeOut(500, () => {
-                expandElementContainer = anime({
-                    targets: '#audio-element',
-                    width: '100%',
-                    backgroundColor: 'rgba(100,100,100, 1.0)',
-                    duration: '1000',
-                    easing: 'linear',
-                    complete: () => controller.appendMinimizer()
-                });
-            });
-        },
+        // expandControls() {
+        //     $('#controls-expander').fadeOut(500, () => {
+        //         expandElementContainer = anime({
+        //             targets: '#audio-element',
+        //             width: '100%',
+        //             backgroundColor: 'rgba(100,100,100, 1.0)',
+        //             duration: '1000',
+        //             easing: 'linear',
+        //             complete: () => controller.appendMinimizer()
+        //         });
+        //     });
+        // },
 
-        appendMinimizer() {
-            $('#audio-element').append(controller.audioElementControlsMinimizer);
-            $('#controls-minimizer').on('click', function(e) {
-                controller.minimizeControls();
-            });
-        },
+        // appendMinimizer() {
+        //     $('#audio-element').append(controller.audioElementControlsMinimizer);
+        //     $('#controls-minimizer').on('click', function(e) {
+        //         controller.minimizeControls();
+        //     });
+        // },
 
-        minimizeControls() {
-            $('#controls-minimizer').fadeOut(500, () => {
-                $('#controls-minimizer').remove();
-                expandElementContainer = anime({
-                    targets: '#audio-element',
-                    width: '0',
-                    backgroundColor: 'rgba(100,100,100, 0)',
-                    duration: '1000',
-                    easing: 'linear',
-                    complete: () => $('#controls-expander').fadeIn()
-                });
-            });
-        },
+        // minimizeControls() {
+        //     $('#controls-minimizer').fadeOut(500, () => {
+        //         $('#controls-minimizer').remove();
+        //         expandElementContainer = anime({
+        //             targets: '#audio-element',
+        //             width: '0',
+        //             backgroundColor: 'rgba(100,100,100, 0)',
+        //             duration: '1000',
+        //             easing: 'linear',
+        //             complete: () => $('#controls-expander').fadeIn()
+        //         });
+        //     });
+        // },
 
         queRandomAudio() {
             var randomStory = audioModel.stories[Math.floor(Math.random() * audioModel.stories.length)];
