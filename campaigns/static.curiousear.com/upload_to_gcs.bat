a994fb0f933fa36286ed31907727d1e3f85b1a0c
Dimitrii Home <dimitrii@engineering-bear.com>
2018-02-07 07:27:49 -0800

    Added Ear App Record Screen for dynamic changes.

--- campaigns/static.curiousear.com/upload_to_gcs.bat
+++ campaigns/static.curiousear.com/upload_to_gcs.bat
@@ -1,5 +1,1 @@
-call gsutil -m rm gs://static.curiousear.com/**
-call gsutil -m cp -c -r . gs://static.curiousear.com
-call gsutil rm gs://static.curiousear.com/upload_to_gcs.bat
-call gsutil rm gs://static.curiousear.com/dev_server.bat
-call gsutil rm gs://static.curiousear.com/index.html
+call gsutil -m rsync -d -r . gs://static.curiousear.com
